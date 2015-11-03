d322c73f8f03fa924926b671c6036f86683c00c1
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2015-11-03 18:10:36 -0500

    app.yaml

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
+var swig  = require('swig');
+var fs  = require('fs');
+
+var tunnel = require('./routes/tunnel');
+var sequest = require('sequest')
 var bodyParser = require('body-parser'),
 	hostname = process.env.HOSTNAME || 'localhost',
     port = parseInt(process.env.PORT, 10) || 3000,
-    publicDir = process.argv[2] || __dirname + '/public';
+    publicDir = process.argv[2] || __dirname ;
 
 var app = express();
-var swig  = require('swig');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(publicDir));
-
-// view engine setup
-// app.set('view engine', 'jade');
-
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(publicDir));
+app.use(express.static(path.join(__dirname, '/public')));
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/img',express.static(path.join(__dirname, 'public/images')));
-// app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
-// app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
+var key = fs.readFileSync(process.env.HOME + '/.ssh/google_compute_engine');
 
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+sequest('radhikadesai@104.197.111.54', {
+  command: 'ls',
+  privateKey: key
+  }, function (err, stdout) {
+    console.log("tunnelPort : ",tunnel())
+	// var connection = mysql.createConnection({
+ //  		  host     : '104.197.111.54',
+	// 	  user     : 'root',
+	// 	  password : 'casino',
+	// 	  database : 'casinonew'
+	// });
+	// connection.connect();
 });
-// app.use('/users', users);
+app.get('/', function(req, res, next) {
+	// console.log("connection object : ",connection);
+	// connection.query('SELECT Name from conference', function(err, rows, fields) {
+ // 	 if (!err)
+ //  	  console.log('The solution is: ', rows);
+ // 	 else
+ //   	 console.log('Error while performing Query.');
+	// });
+  	res.render('index', { pagename: 'Casino',
+    authors: ['Paul', 'Jim', 'Jane']});
+});	
+// app.use('/users', users)
 
 module.exports = app;
 app.listen(port, hostname);
