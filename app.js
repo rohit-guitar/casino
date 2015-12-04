967c6de0ae88db0c5868f9cc91a64389b7097505
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2015-12-04 16:26:21 -0500

    allowing cross domain

--- app.js
+++ app.js
@@ -12,16 +12,37 @@ var bodyParser = require('body-parser'),
     port = 3000,
     publicDir = process.argv[2] || __dirname ;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 var app = express();
-app.use(bodyParser.json());
+
+  // app.use(express.bodyParser());
+  // app.use(express.methodOverride());
+  app.use(allowCrossDomain);
+  // app.use(app.router);
+  app.use(express.static(publicDir));
+  app.use(express.static(path.join(__dirname, '/public')));
+  // app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(publicDir));
-app.use(express.static(path.join(__dirname, '/public')));
+
 
 var con = mysql.createConnection({
   host: "173.194.232.115",
@@ -149,5 +170,6 @@ app.get("/teams",function(req,res,next){
 
 
 module.exports = app;
+console.log("Environment port is : ",process.env.PORT);
 app.listen(process.env.PORT || 3000);
 
