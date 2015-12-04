67675f19b1dbd93e5cdb548a35098fb103d4fc1c
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2015-12-04 15:06:57 -0500

    Changes to top widgets on homepage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,19 +4,40 @@
 			type: "GET",
 			url: baseURL+ "/index/userbets",
 		}).done(function( data ) {
-	   		console.log(data);
 	   	    datar=data;
 	   		var table1 = document.getElementById("user-bets");
-
+	   		
+	   		var pointSpreadBets=0;
+	   		var totalBets = 0; 
+	   		var moneyLineBets=0;
 	   		data.forEach(function(datarow){
+	   			if(datarow["Bet Type"] == "Point Spread"){
+	   				pointSpreadBets = pointSpreadBets+1;
+	   			}
+	   			if(datarow["Bet Type"] == "Total Bets"){
+	   				totalBets = totalBets +1
+	   			}
+	   			if(datarow["Bet Type"] == "Win/Loss")
+	   			{
+	   				moneyLineBets  = moneyLineBets +1;
+	   			}
+	   		});
+	   		$("#money-bets").text(moneyLineBets);
+	   		$("#point-spread-bets").text(pointSpreadBets);
+	   		$("#total-bets").text(totalBets);
+	   		console.log("moneyLineBets pointSpreadBets totalBets ", moneyLineBets," ",pointSpreadBets," ",totalBets);
+	   		var totalBetsPlaced = moneyLineBets+pointSpreadBets+totalBets;
+			$("#total-bets-placed").text(totalBetsPlaced);
+	   		for(var i=0 ; i<7; i++){
 	   			var row = table1.insertRow(1);
 				var cell1 = row.insertCell(0);
 				var cell2 = row.insertCell(1);
 				var cell3 = row.insertCell(2);
-				cell1.innerHTML = datarow.Date;
-				cell2.innerHTML = datarow["Bet Type"];
-				cell3.innerHTML = datarow.Profit;
-	   		})
+				cell1.innerHTML = data[i].Date;
+				cell2.innerHTML = data[i]["Bet Type"];
+				cell3.innerHTML = data[i].Profit;
+
+	   		}
 		});;
 	
 
@@ -45,8 +66,10 @@
 		// 	success: jobSubmitSuccess,
 		// 	error : jobSubmitError
 		// });
-	$.post("http://130.211.186.99:8998/batches",{ "file" : "hdfs://130.211.186.99:9000/user/dl/spark-scala-maven-project-0.0.1-SNAPSHOT-jar-with-dependencies.jar",
-				    "className" : "SampleApp",
-				    "args":"["+date+"]"},jobSubmitSuccess);
+		$.get("http://130.211.186.99/test.php",jobSubmitSuccess);
+
+		$.post("http://130.211.186.99:8998/batches",{ "file" : "hdfs://130.211.186.99:9000/user/dl/spark-scala-maven-project-0.0.1-SNAPSHOT-jar-with-dependencies.jar",
+					    "className" : "SampleApp",
+					    "args":"["+date+"]"},jobSubmitSuccess);
 	});
 })();
